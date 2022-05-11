5d354e079c65dc0d36aadd34cda42dc9c3347a1d
Diego Grisales <dfgrisales5078@eagle.fgcu.edu>
2022-05-10 17:28:14 -0700

    Objects

--- notes.js
+++ notes.js
@@ -634,7 +634,7 @@ const smallerNums = nums.map(num => num - 5);
 // Choose a method that will return a boolean value
 nums.some(num => num < 0);
 
-*/
+
 
 
 // iterators practice - Grammar Checker
@@ -711,6 +711,175 @@ storyWords.forEach(word => {
 })
 console.log(count1);
 
+
+*/
+
+
+// ------------------- End - Iterators -----------------------
+
+
+// ----------------------- Objects ---------------------------
+
+
+// Write your fasterShip object literal b
+/*
+let spaceship1= {
+    homePlanet: 'Earth',
+    color: 'silver',
+    'Fuel Type': 'Turbo Fuel',
+    numCrew: 5,
+    flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+let crewCount = spaceship1.numCrew;
+let planetArray = spaceship1.flightPath;
+
+let propName =  'Active Mission';
+
+// Write your code below
+let isActive = spaceship1['Active Mission']
+
+//console.log(spaceship1['Active Mission'])
+
+
+let spaceship2 = {
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth',
+    color: 'silver',
+    'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+//Reassign the color property of the spaceship object to have a value of 'glorious gold'
+spaceship2.color = 'glorious gold';
+spaceship2.numEngines = 8;
+delete spaceship2["Secret Mission"];
+
+
+// object functions
+
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+// Write your code below
+const alienShip = {
+    retreat() {
+        console.log(retreatMessage)
+    },
+    takeOff() {
+        console.log('Spim... Borp... Glix... Blastoff!')
+    }
+};
+
+alienShip.retreat();
+alienShip.takeOff();
+*/
+
+//Nested Objects:
+/*
+let spaceship = {
+    passengers: null,
+    telescope: {
+        yearBuilt: 2018,
+        model: "91031-XLT",
+        focalLength: 2032
+    },
+    crew: {
+        captain: {
+            name: 'Sandra',
+            degree: 'Computer Engineering',
+            encourageTeam() { console.log('We got this!') },
+            'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+    },
+    engine: {
+        model: "Nimbus2000"
+    },
+    nanoelectronics: {
+        computer: {
+            terabytes: 100,
+            monitors: "HD"
+        },
+        'back-up': {
+            battery: "Lithium",
+            terabytes: 50
+        }
+    }
+};
+
+let capFave = spaceship.crew.captain["favorite foods"][0];
+spaceship.passengers = [{name: 'Diego'}, {name: 'Belen'}]
+let firstPassenger = spaceship.passengers[0];
+*/
+
+/*
+// Pass By Reference
+
+let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth'
+};
+
+// Write your code below
+let greenEnergy = obj => {
+    obj['Fuel Type'] = 'avocado oil';
+};
+
+let remotelyDisable = obj => {
+    obj.disabled = true;
+};
+
+// pass an object to a function
+greenEnergy(spaceship);
+remotelyDisable(spaceship);
+console.log(spaceship);
+*/
+
+
+//Looping Through Objects
+/*
+let spaceship = {
+    crew: {
+        captain: {
+            name: 'Lily',
+            degree: 'Computer Engineering',
+            cheerTeam() { console.log('You got this!') }
+        },
+        'chief officer': {
+            name: 'Dan',
+            degree: 'Aerospace Engineering',
+            agree() { console.log('I agree, captain!') }
+        },
+        medic: {
+            name: 'Clementine',
+            degree: 'Physics',
+            announce() { console.log(`Jets on!`) } },
+        translator: {
+            name: 'Shauna',
+            degree: 'Conservation Science',
+            powerFuel() { console.log('The tank is full!') }
+        }
+    }
+};
+
+// Write your code below
+// print crew role and names of all crew members using for ... in
+for (let crewMember in spaceship.crew) {
+    console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`)
+}
+
+// print each crew members name and degree
+for (let crewMember in spaceship.crew) {
+    console.log(`${spaceship.crew[crewMember]. name}: ${spaceship.crew[crewMember].degree}`)
+}
+
+*/
+
+//
+
+
+// ----------------------- End - Objects ---------------------------
+
+
+
+// ---------------------- Advanced Objects -------------------------
 
 
 
